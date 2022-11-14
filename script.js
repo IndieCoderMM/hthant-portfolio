0690570917ebc304cc1b8886d4e66adbd48afb2e
hthant <hthant00chk@gmail.com>
2022-11-14 22:17:17 +0630

    Add toggle menu feature

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const hamburgerOpen = document.querySelector('#hamburger-menu');
+const hamburgerMenu = document.querySelector('#hamburger-menu');
 const hamburgerImg = document.querySelector('#hamburger-img');
+const menuItems = hamburgerMenu.querySelectorAll('a');
 
-hamburgerImg.addEventListener('click', () => {
-  hamburgerOpen.classList.add('hamburger-list');
-});
+function toggleMenu() {
+  hamburgerMenu.classList.toggle('hamburger-list');
+  if (hamburgerImg.classList.contains('hamburger')) {
+    hamburgerImg.src = 'img/icons/close-icon.png';
+    hamburgerImg.classList.remove('hamburger');
+  } else {
+    hamburgerImg.src = 'img/icons/hamburger.png';
+    hamburgerImg.classList.add('hamburger');
+  }
+}
+
+hamburgerImg.addEventListener('click', toggleMenu);
+menuItems.forEach((item) =>
+  item.addEventListener('click', () => {
+    if (hamburgerMenu.classList.contains('hamburger-list')) {
+      toggleMenu();
+    }
+  })
+);
